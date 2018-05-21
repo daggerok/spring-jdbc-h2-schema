28de803db09ae80fcafe163149b28bc6d9f4b1b5
Maksim Kostromin <daggerok@gmail.com>
2018-05-22 00:27:47 +0300

    Use default schema.sql and data.sql files locations

--- src/main/java/daggerok/jdbc/DataSourceConfig.java
+++ src/main/java/daggerok/jdbc/DataSourceConfig.java
@@ -3,18 +3,12 @@ package daggerok.jdbc;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
-import javax.sql.DataSource;
 import java.util.Collection;
 import java.util.Map;
 
 import static java.lang.String.format;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Configuration
 class DataSourceConfig {
@@ -27,19 +21,5 @@ class DataSourceConfig {
         .flatMap(Collection::parallelStream)
         .map(e -> format("%s: %s", e.getKey(), e.getValue()))
         .forEach(System.out::println);
-  }
-
-  @Bean
-  public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
-    final DataSourceInitializer initializer = new DataSourceInitializer();
-    initializer.setDataSource(dataSource);
-    initializer.setDatabasePopulator(databasePopulator());
-    return initializer;
-  }
-
-  private DatabasePopulator databasePopulator() {
-    final ClassPathResource schema = new ClassPathResource("sql/ddl/schema.sql", DataSourceConfig.class.getClassLoader());
-    final ClassPathResource data = new ClassPathResource("sql/dml/data.sql", DataSourceConfig.class.getClassLoader());
-    return new ResourceDatabasePopulator(false, true, UTF_8.displayName(), schema, data);
   }
 }
